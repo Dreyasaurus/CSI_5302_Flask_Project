4116f74c98d18994a6f38259afc8f5480ba974fb
billy bissontz <bbissontz@homtail.com>
2022-10-29 14:46:12 -0700

    login required capabilities

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from app import app, db
 from flask import render_template, redirect, url_for, request, flash
 from app.forms import LoginForm, RegistrationForm
-
-from flask_login import current_user, login_user, logout_user
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -19,7 +20,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user,posts=posts)
+    return render_template("index.html", title='Home Page', posts=posts)
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -34,7 +35,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
     
     
